012ebe371fc3527a9b6066f7687cb95e0d8c8009
Ezequiel Monforte <ezequielmonforte@gmail.com>
2020-06-24 14:15:15 -0300

    Initial commit

---
+++ SharedModels/StreamingService/ConferenceRecordFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModels.StreamingService
+{
+	class ConferenceRecordFile: EntityBase
+	{
+		public IUser Owner { get; set; }
+		public string Path { get; set; }
+	}
+}
---
+++ SharedModels/StreamingService/VideoRoom.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedModels.StreamingService
+{
+	public class VideoRoom : EntityBase
+	{
+		public List<IUser> Users { get; set; }
+		public IUser Owner { get; set; }
+		public string Name { get; set; }
+		public TimeSpan Duration { get; set; }
+
+	}
+}
