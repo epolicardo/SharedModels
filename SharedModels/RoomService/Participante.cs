6701ebae14f6a8304c9602148bb89b9a47aca946
Diego Enrique Henderson <diegohenderson425@outlook.com>
2020-07-01 21:26:03 -0300

    Creacion de Modelos

---
+++ SharedModels/RoomService/Participante.cs
@@ -0,0 +1,24 @@
+using SharedModels;
+using System;
+using System.Collections.Generic;
+
+public class Participante:EntityBase
+{
+    public int Id_Participante { get; set; }
+    public string Email { get; set; }
+    public string Token { get; set; }
+    public DateTime FechaAlta { get; set; }
+    public DateTime FechaBaja { get; set; }
+    public List<Asistencia> Asistencia { get; set; }
+    public List<Sala> Salas { get; set; }
+    public Roles Rol { get; set; }
+    
+}
+public enum Roles
+{
+    Propietario = 1,
+    Administrador = 2,
+    Invitado = 3,
+    Oyente = 4
+
+};
